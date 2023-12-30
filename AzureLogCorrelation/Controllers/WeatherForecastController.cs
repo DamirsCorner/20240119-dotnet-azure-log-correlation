6089bcb06ffc5f25656878a45676ed64e0110b11
Damir Arh <damir.arh@gmail.com>
2023-12-30 09:08:44 +0100

    Add project files.

---
+++ AzureLogCorrelation/Controllers/WeatherForecastController.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureLogCorrelation.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing",
+        "Bracing",
+        "Chilly",
+        "Cool",
+        "Mild",
+        "Warm",
+        "Balmy",
+        "Hot",
+        "Sweltering",
+        "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        var forecast = Enumerable
+            .Range(1, 5)
+            .Select(
+                index =>
+                    new WeatherForecast
+                    {
+                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        TemperatureC = Random.Shared.Next(-20, 55),
+                        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                    }
+            )
+            .ToArray();
+
+        _logger.LogInformation(
+            "Date: {Date}, Temperature: {Temperature}",
+            forecast[0].Date,
+            forecast[0].TemperatureC
+        );
+
+        return forecast;
+    }
+}
---
+++ AzureLogCorrelation/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.HttpLogging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddApplicationInsightsTelemetry();
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = HttpLoggingFields.All;
+    options.CombineLogs = true;
+});
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseHttpLogging();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
